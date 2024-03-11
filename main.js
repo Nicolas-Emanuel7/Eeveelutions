e30df99cfc164bc478e06e8219fcbbd4fbb94aec
nicolas <niculas20003@outlook.com>
2024-03-11 13:56:05 -0300

    botão pros modelos

--- main.js
+++ main.js
@@ -2,12 +2,12 @@ import './style.css'
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { CSS2DRenderer, CSS2DObject } from 'three/examples/jsm/renderers/CSS2DRenderer.js';
 
 // Cena
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
-const bodyElement = document.querySelector('body')
 
 // Define o tamanho da janela do navegador
 const sizes = {
@@ -49,20 +49,105 @@ directionalLight.castShadow = true
 // Adiciona a luz direcional à cena
 scene.add(directionalLight)
 
-// Variáveis
-
 // Modelos
 
-// Adicione um ouvinte de eventos para cada botão
-document.getElementById("botaoEevee").addEventListener("click", function() {
-  console.log('Carregando modelo Eevee')
-  carregarModelo('eevee');
-});
+//2dRenderer
+const botaoRenderer = new CSS2DRenderer();
+botaoRenderer.setSize(window.innerWidth, window.innerHeight);
+botaoRenderer.domElement.style.position = 'absolute';
+botaoRenderer.domElement.style.top = '0px'
+document.body.appendChild(botaoRenderer.domElement);
 
-document.getElementById("botaoEspeon").addEventListener("click", function() {
-  console.log('Carregando modelo Espeon')
-  carregarModelo('espeon');
+
+// BOTÃO EEVEE
+const botaoEevee = document.createElement('button');
+botaoEevee.textContent = 'Eevee';
+botaoEevee.addEventListener('click', () => {
+  carregarModelos('eevee');
 });
+const botaoEeveeObj = new CSS2DObject(botaoEevee);
+botaoEeveeObj.position.set(0, -2, 0);
+scene.add(botaoEeveeObj);
+
+// BOTÃO ESPEON
+const botaoEspeon = document.createElement('button');
+botaoEspeon.textContent = 'Espeon';
+botaoEspeon.addEventListener('click', () => {
+  carregarModelos('espeon');
+});
+const botaoEspeonObj = new CSS2DObject(botaoEspeon);
+botaoEspeonObj.position.set(1, -2, 0);
+scene.add(botaoEspeonObj);
+
+// BOTÃO FLAREON
+const botaoFlareon = document.createElement('button');
+botaoFlareon.textContent = 'Espeon';
+botaoFlareon.addEventListener('click', () => {
+  carregarModelos('flareon');
+});
+const botaoFlareonObj = new CSS2DObject(botaoFlareon);
+botaoFlareonObj.position.set(2, -2, 0);
+scene.add(botaoFlareonObj);
+
+// BOTÃO GLACEON
+const botaoGlaceon = document.createElement('button');
+botaoGlaceon.textContent = 'Glaceon';
+botaoGlaceon.addEventListener('click', () => {
+  carregarModelos('glaceon');
+});
+const botaoGlaceonObj = new CSS2DObject(botaoGlaceon);
+botaoGlaceonObj.position.set(-1, -2, 0);
+scene.add(botaoGlaceonObj);
+
+// BOTÃO JOLTEON
+const botaoJolteon = document.createElement('button');
+botaoJolteon.textContent = 'Jolteon';
+botaoJolteon.addEventListener('click', () => {
+  carregarModelos('jolteon');
+});
+const botaoJolteonObj = new CSS2DObject(botaoJolteon);
+botaoJolteonObj.position.set(-2, -2, 0);
+scene.add(botaoJolteonObj);
+
+// BOTÃO LEAFEON
+const botaoLeafeon = document.createElement('button');
+botaoLeafeon.textContent = 'Leafeon';
+botaoLeafeon.addEventListener('click', () => {
+  carregarModelos('leafeon');
+});
+const botaoLeafeonObj = new CSS2DObject(botaoLeafeon);
+botaoLeafeonObj.position.set(-3, -2, 0);
+scene.add(botaoLeafeonObj);
+
+// BOTÃO SYLVEON
+const botaoSylveon = document.createElement('button');
+botaoSylveon.textContent = 'Sylveon';
+botaoSylveon.addEventListener('click', () => {
+  carregarModelos('sylveon');
+});
+const botaoSylveonObj = new CSS2DObject(botaoSylveon);
+botaoSylveonObj.position.set(3, -2, 0);
+scene.add(botaoSylveonObj);
+
+// BOTÃO UMBREON
+const botaoUmbreon = document.createElement('button');
+botaoUmbreon.textContent = 'Umbreon';
+botaoUmbreon.addEventListener('click', () => {
+  carregarModelos('umbreon');
+});
+const botaoUmbreonObj = new CSS2DObject(botaoUmbreon);
+botaoUmbreonObj.position.set(-4, -2, 0);
+scene.add(botaoUmbreonObj);
+
+// BOTÃO VAPOREON
+const botaoVaporeon = document.createElement('button');
+botaoVaporeon.textContent = 'Vaporeon';
+botaoVaporeon.addEventListener('click', () => {
+  carregarModelos('vaporeon');
+});
+const botaoVaporeonObj = new CSS2DObject(botaoVaporeon);
+botaoVaporeonObj.position.set(4, -2, 0);
+scene.add(botaoVaporeonObj);
 
 var eeveeModel, espeonModel, flareonModel, glaceonModel, jolteonModel, leafeonModel, sylveonModel, umbreonModel, vaporeonModel;
 
@@ -112,7 +197,6 @@ function carregarModelos(modelo) {
     Eevee.load('/assets/eevee/scene.gltf', (eevee) => {
       eevee.scene.scale.set(8, 8, 8);
       scene.add(eevee.scene);
-      eevee.scene.position.x = 0;
       eevee.scene.castShadow = true;
       eeveeModel = eevee.scene;
     });
@@ -122,70 +206,69 @@ function carregarModelos(modelo) {
     Espeon.load('/assets/espeon/scene.gltf', (espeon) => {
       espeon.scene.scale.set(2, 2, 2);
       scene.add(espeon.scene);
-      espeon.scene.position.x = -20;
       espeon.scene.castShadow = true;
       espeonModel = espeon.scene;
     });
   } else if (modelo === 'flareon') {
+    console.log('Carregando modelo Flareon')
   const Flareon = new GLTFLoader();
   Flareon.load('/assets/flareon/scene.gltf', (flareon) => {
     flareon.scene.scale.set(2,2,2);
     scene.add(flareon.scene);
-    flareon.scene.position.x = -15;
     flareon.scene.castShadow = true
     flareonModel = flareon.scene;
   });
   } else if (modelo === 'glaceon') {
+    console.log('Carregando modelo Glaceon')
   const Glaceon = new GLTFLoader();
   Glaceon.load('/assets/glaceon/scene.gltf', (glaceon) => {
     glaceon.scene.scale.set(2,2,2);
     scene.add(glaceon.scene);
-    glaceon.scene.position.x = 0;
     glaceon.scene.castShadow = true
     glaceonModel = glaceon.scene;
   });
   } else if (modelo === 'jolteon') {
+    console.log('Carregando modelo Jolteon')
   const Jolteon = new GLTFLoader();
   Jolteon.load('/assets/jolteon/scene.gltf', (jolteon) => {
     jolteon.scene.scale.set(2,2,2);
     scene.add(jolteon.scene);
-    jolteon.scene.position.x = -5;
     jolteon.scene.castShadow = true
     jolteonModel = jolteon.scene;
   });
   } else if (modelo === 'leafeon') {
+    console.log('Carregando modelo Leafeon')
   const Leafeon = new GLTFLoader();
   Leafeon.load('/assets/leafeon/scene.gltf', (leafeon) => {
     leafeon.scene.scale.set(2,2,2);
     scene.add(leafeon.scene);
-    leafeon.scene.position.x = -10;
     leafeon.scene.castShadow = true
     leafeonModel = leafeon.scene;
   });
   } else if (modelo === 'sylveon') {
+    console.log('Carregando modelo Sylveon')
   const Sylveon = new GLTFLoader();
   Sylveon.load('/assets/SYLVEON/scene.gltf', (sylveon) => {
     sylveon.scene.scale.set(2,2,2);
     scene.add(sylveon.scene);
-    sylveon.scene.position.x = 5;
     sylveon.scene.castShadow = true
     sylveonModel = sylveon.scene;
   });
   } else if (modelo === 'umbreon') {
+    console.log('Carregando modelo Umbreon')
   const Umbreon = new GLTFLoader();
   Umbreon.load('/assets/umbreon/scene.gltf', (umbreon) => {
     umbreon.scene.scale.set(2,2,2);
     scene.add(umbreon.scene);
-    umbreon.scene.position.x = 10;
     umbreon.scene.castShadow = true
     umbreonModel = umbreon.scene;
   });
   } else if (modelo === 'vaporeon') {
+    
   const Vaporeon = new GLTFLoader();
   Vaporeon.load('/assets/vaporeon/scene.gltf', (vaporeon) => {
     vaporeon.scene.scale.set(2,2,2);
     scene.add(vaporeon.scene);
-    vaporeon.scene.position.x = 15;
     vaporeon.scene.castShadow = true
     vaporeonModel = vaporeon.scene;
   });
@@ -193,47 +276,7 @@ function carregarModelos(modelo) {
 }
 carregarModelos();
 
-/*
-function criarFlocoDeNeve() {
-  const flocoDeNeve = new GLTFLoader();
-  flocoDeNeve.load('/assets/Neve/scene.gltf', (snowflake) => {
-      snowflake.scene.scale.set(0.01, 0.01, 0.01);
 
-      // Define a posição inicial aleatória dentro da área específica
-      snowflake.scene.position.set(
-          Math.random() * 10 - 5, // valor entre -5 e 5
-          10, // altura inicial
-          Math.random() * 10 - 5 // valor entre -5 e 5
-      );
-
-      scene.add(snowflake.scene);
-
-      // Função para atualizar a posição do floco de neve
-      function atualizarFlocoDeNeve() {
-          // Atualiza a posição do floco de neve para simular a queda
-          snowflake.scene.position.y -= 0.05; // velocidade de queda
-
-          // Se o floco de neve cair abaixo de uma certa altura, reinicie sua posição
-          if (snowflake.scene.position.y < -5) {
-              snowflake.scene.position.set(
-                  Math.random() * 10 - 5, // valor entre -5 e 5
-                  10, // altura inicial
-                  Math.random() * 10 - 5 // valor entre -5 e 5
-              );
-          }
-
-          // Chama essa função novamente no próximo quadro
-          requestAnimationFrame(atualizarFlocoDeNeve);
-      }
-
-      // Inicia a atualização do floco de neve
-      atualizarFlocoDeNeve();
-  });
-}
-
-// Chama a função para criar flocos de neve periodicamente
-setInterval(criarFlocoDeNeve, 2000); // Gera um novo floco de neve a cada segundo
-*/
 
 const ground = new THREE.Mesh( new THREE.CylinderGeometry(3,3,2,32) , new THREE.MeshStandardMaterial( { color: 0x00ffff, metalness:0.25 } ) );
 scene.add( ground );
@@ -256,22 +299,47 @@ function onMouseMove(event) {
   var rotationY = mouseX * Math.PI * 0.2;
 
   // Aplicar as rotações aos modelos
-  if (eeveeModel) {
-      eeveeModel.rotation.y = rotationY;
+  if(eeveeModel){
+    eeveeModel.rotation.y = rotationY;
   }
+  if(espeonModel){
+    espeonModel.rotation.y = rotationY;
+  }
+  if(flareonModel){
+    flareonModel.rotation.y = rotationY;
+  }
+  if(glaceonModel){
+    glaceonModel.rotation.y = rotationY;
+  }
+  if(jolteonModel){
+    jolteonModel.rotation.y = rotationY;
+  }
+  if(leafeonModel){
+    leafeonModel.rotation.y = rotationY;
+  }
+  if(sylveonModel){
+    sylveonModel.rotation.y = rotationY;
+  }
+  if(umbreonModel){
+    umbreonModel.rotation.y = rotationY;
+  }
+  if(vaporeonModel){
+    vaporeonModel.rotation.y = rotationY;
+  }
+
   if(ground){
     ground.rotation.y = rotationY;
   
   }
 }
 
-
-
 // ANIMAÇÃO
 
 function animate(){
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
+
+    botaoRenderer.render(scene, camera);
 
 }
 
